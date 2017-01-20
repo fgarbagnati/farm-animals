2962c0377ceb601fbbcad68d44e3017b1f2ca28b
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-20 13:47:48 -0800

    Adds basic tweening between animal switching

--- js/main.js
+++ js/main.js
@@ -71,15 +71,22 @@ var GameState = {
 
 		if(sprite.customParams.direction > 0) {
 			newAnimal = this.animals.next();
+			newAnimal.x = -newAnimal.width/2;
 			endX = 640 + this.currentAnimal.width/2;
 		} else {
 			newAnimal = this.animals.previous();
+			newAnimal.x = 640 + newAnimal.width/2;
 			endX = -this.currentAnimal.width/2;
 		}
 
-		this.currentAnimal.x = endX; 
+		var newAnimalMovement = this.game.add.tween(newAnimal);
+		newAnimalMovement.to({x: this.game.world.centerX}, 1000);
+		newAnimalMovement.start();
 
-		newAnimal.x = this.game.world.centerX;
+		var currentAnimalMovement = this.game.add.tween(this.currentAnimal);
+		currentAnimalMovement.to({x: endX}, 1000);
+		currentAnimalMovement.start();
+
 		this.currentAnimal = newAnimal; 
 	},
 	animateAnimal: function(sprite, event) {
