e41c8041402e8bc47cf560615964194eaeb8f462
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-20 12:24:36 -0800

    Adds comments and rearranges js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
-
+// game will have only one state
 var GameState = {
+	// load the game assets before game starts
 	preload: function() {
 
 	},
+	// executed after everything is loaded
 	create: function() {
 
 	},
+	// executed multiple times per second
 	update: function() {
 
 	}
 };
+
+// initiate the Phaser framework
+var game = new Phaser.Game(640, 360, Phaser.AUTO);
 
 game.state.add('GameState', GameState);
 game.state.start('GameState');
