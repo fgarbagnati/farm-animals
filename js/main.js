c4080904af7fbf1ef66430f3f459cbaaa3f49959
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-20 13:10:04 -0800

    Allows user input on both arrows and creates a switchAnimal function

--- js/main.js
+++ js/main.js
@@ -25,15 +25,27 @@ var GameState = {
 		this.rightArrow = this.game.add.sprite(580, this.game.world.centerY, 'arrow');
 		this.rightArrow.anchor.setTo(0.5);
 		this.rightArrow.customParams = {direction: 1};
+		// right arrow allow user input
+		this.rightArrow.inputEnabled = true;
+		this.rightArrow.input.pixelPerfectClick = true;
+		this.rightArrow.events.onInputDown.add(this.switchAnimal, this);
 
 		this.leftArrow = this.game.add.sprite(60, this.game.world.centerY, 'arrow');
 		this.leftArrow.anchor.setTo(0.5);
 		this.leftArrow.scale.x = -1;
 		this.leftArrow.customParams = {direction: -1};
+		// left arrow allow user input
+		this.leftArrow.inputEnabled = true;
+		this.leftArrow.input.pixelPerfectClick = true;
+		this.leftArrow.events.onInputDown.add(this.switchAnimal, this);
 
-	},
+	}, 
 	// executed multiple times per second
 	update: function() {
+
+	},
+	switchAnimal: function(sprite, event) {
+		console.log('move animal');
 	}
 };
 
