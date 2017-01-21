fb0657f385b469d8c3910107954dad28b0e002cd
Francesca Garbagnati <f.g.garbagnati@gmail.com>
2017-01-21 15:54:09 -0800

    Adds audio files to preload function

--- js/main.js
+++ js/main.js
@@ -9,6 +9,11 @@ var GameState = {
 		this.load.spritesheet('horse', 'assets/images/horse_spritesheet.png', 212, 200, 3);
 		this.load.spritesheet('pig', 'assets/images/pig_spritesheet.png', 297, 200, 3);
 		this.load.spritesheet('sheep', 'assets/images/sheep_spritesheet.png', 244, 200, 3);
+
+		this.load.audio('chickenSound', ['assets/audio/chicken.ogg', 'assets/audio/chicken.mp3']);
+		this.load.audio('horseSound', ['assets/audio/horse.ogg', 'assets/audio/horse.mp3']);
+		this.load.audio('pigSound', ['assets/audio/pig.ogg', 'assets/audio/pig.mp3']);
+		this.load.audio('sheepSound', ['assets/audio/sheep.ogg', 'assets/audio/sheep.mp3']);
 	},
 	// executed after everything is loaded
 	create: function() {
